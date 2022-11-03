c71b434bea3f57ca96a90a4bce66814500a966a2
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-11-03 23:23:58 +0100

    Section 5 starting : Express middlewares

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { success } = require('./helper')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -6,13 +7,16 @@ const app = express()
 const port = 8000
 
 app.get('/api/pokemons', (req, res) => {
-  res.send(`il y as actuellement ${pokemons.length} dans le pokédex.`)
+  const pokemonList = pokemons
+  const message = `${pokemonList.length} pokémons trouvé`
+  res.json(success(message, pokemons))
 })
 
 app.get('/api/pokemons/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const pokemon = pokemons.find((pokemon) => pokemon.id === id)
-  res.send(`Pokémon n°${id} : ${pokemon.name}`)
+  const message = 'Un pokémon a bien été trouvé'
+  res.json(success(message, pokemon))
 })
 
 app.listen(port, () =>
