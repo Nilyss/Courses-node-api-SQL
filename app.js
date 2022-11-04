916bf41863ab3e3be3e0ec71ccfda7f4d77194d4
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-11-04 10:25:16 +0100

    Section 7 starting : db SQL

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
-const { success } = require('./helper')
+const bodyParser = require('body-parser')
+const { success, getUniqueId } = require('./helper')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
 
 const port = 8000
 
-app.use(favicon(__dirname + '/favicon.ico')).use(morgan('dev'))
+app
+  .use(favicon(__dirname + '/favicon.ico'))
+  .use(morgan('dev'))
+  .use(bodyParser.json())
 
 app.get('/api/pokemons', (req, res) => {
   const pokemonList = pokemons
@@ -21,6 +25,32 @@ app.get('/api/pokemons/:id', (req, res) => {
   const pokemon = pokemons.find((pokemon) => pokemon.id === id)
   const message = 'Un pokémon a bien été trouvé'
   res.json(success(message, pokemon))
+})
+
+app.post('/api/pokemons', (req, res) => {
+  const id = getUniqueId(pokemons)
+  const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } }
+  pokemons.push(pokemonCreated)
+  const message = `le pokémon ${pokemonCreated.name} a bien été crée. `
+  res.json(success(message, pokemonCreated))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonUpdated = { ...req.body, id: id }
+  pokemons = pokemons.map((pokemon) => {
+    return pokemon.id === id ? pokemonUpdated : pokemon
+  })
+  const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié`
+  res.json(success(message, pokemonUpdated))
+})
+
+app.delete('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonDeleted = pokemons.find((pokemon) => pokemon.id === id)
+  pokemons = pokemons.filter((pokemon) => pokemon.id !== id)
+  const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé`
+  res.json(success(message, pokemonDeleted))
 })
 
 app.listen(port, () =>
