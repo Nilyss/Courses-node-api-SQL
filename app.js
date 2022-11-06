24923d4889fec576f27e984e56aaad00c9b45d78
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-11-06 18:09:37 +0100

    Section 13 starting : deploy API

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ require('./src/routes/findPokemonByPk')(app)
 require('./src/routes/createPokemon')(app)
 require('./src/routes/updatePokemon')(app)
 require('./src/routes/deletePokemon')(app)
+require('./src/routes/login')(app)
 
 // handle 404 error
 app.use(({ res }) => {
